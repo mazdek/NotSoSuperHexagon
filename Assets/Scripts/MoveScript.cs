0f1ec49e4d7d438f2e05315334dd57db0a1024e4
mazdek <baskn23@hotmail.com>
2021-07-27 23:45:57 +0300

    Directory changes and some minor fixes

---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MoveScript : MonoBehaviour
+{
+    public GameObject target;
+
+    void Update()
+    {
+        // Spin the object around the target at 20 degrees/second.
+        transform.RotateAround(target.transform.position, Vector3.back, 6f*Input.GetAxisRaw("Horizontal"));
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(0);
+    }
+}
